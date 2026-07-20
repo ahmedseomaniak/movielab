1fbb8ace111afa227edb1d72c3a6c73791a4182a
ahmedseomaniak <ahmedseomaniak@users.noreply.github.com>
2026-07-20 10:15:00 +0100

    chore: scaffold Spring Boot project

---
+++ server/src/main/java/com/movielab/MovielabApplication.java
@@ -0,0 +1,11 @@
+package com.movielab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovielabApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MovielabApplication.class, args);
+    }
+}
